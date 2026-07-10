a02275868a95ad28c308f89028c47637f3b67127
KodehodeTor <torjarand.26pg@gmail.com>
2026-07-10 10:23:34 +0200

    Added dice roller to html and css, added also constant variables from dice into javascript.

---
+++ script.js
@@ -0,0 +1,6 @@
+//  DICE ROLLER:
+
+const dice1 = document.getElementById("dice1");
+const dice2 = document.getElementById("dice2");
+const button = document.getElementById("roll-btn");
+const total = document.getElementById("total");
