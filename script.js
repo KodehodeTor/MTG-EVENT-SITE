0c533899fedaa47edd4b6ee68975a319e1acad06
KodehodeTor <torjarand.26pg@gmail.com>
2026-07-10 10:27:20 +0200

    Started function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 //  DICE ROLLER:
 
+// Constant reference variables:
+
 const dice1 = document.getElementById("dice1");
 const dice2 = document.getElementById("dice2");
 const button = document.getElementById("roll-btn");
 const total = document.getElementById("total");
+
+// Event listener for next function
+button.addEventListener("click", rollDice);
+
+// Function for the dice roller 
+function rollDice() {
+    //Animation classes
+  dice1.classList.add("rolling");
+  dice2.classList.add("rolling");
